4f57d23c3363ddc73a448a588b27784be98981a4
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2021-06-17 13:00:15 +0700

    upload react js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Index from "pages/Index";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
