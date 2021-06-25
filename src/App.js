4efd2476fcc2a68512dad1a4df2aa646c7d9af6e
ajiwahyugi.git <ajiwahyugi.git@gmail.com>
2021-06-25 15:26:44 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
-import Index from "pages/Index";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Index from './pages/Index';
+import "bootstrap/dist/css/bootstrap.css";
+
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <Router>
+       <Router>
         <Switch>
-          
+
+          <Route exact path="/" component={Index} />
+
         </Switch>
-      </Router>
+    </Router>
     </div>
+
   );
 }
 
